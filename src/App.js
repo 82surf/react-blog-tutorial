2ae06491c0e0d22d8161e187225ceddc891e8e4f
82surf <82surf@gmail.com>
2021-12-22 18:50:54 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Modal from './components/Modal'
 
 function App() {
+
+  let [titles, updateTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학'])
+
+  let [likes, incrementLikes] = useState(0);
+
+  function changeTitle() {
+    const tmpTitles = titles.slice();
+    tmpTitles[0] = '여자 코트 추천'
+    updateTitle(tmpTitles);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* NAVBAR */}
+      <div className="black-nav">
+        <div>My-blog</div>
+      </div>
+      <button onClick={ changeTitle }>Chage Title</button>
+      {/* LIST */}
+      <div className="list">
+        <h3>
+          { titles[0] }
+          <span onClick={ () => { incrementLikes(++likes) } }>👍</span>
+          { likes }
+        </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{ titles[1] }</h3>
+        <p>2월 18일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{ titles[2] }</h3>
+        <p>2월 19일 발행</p>
+        <hr />
+      </div>
+
+      <Modal />
     </div>
   );
 }
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Modal.css"
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세 내용</p>
+    </div>
+  );
+};
+
+export default Modal
